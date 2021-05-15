e585750b34f5f98f7b5a4a089af7d5e97cda1fb0
Kevin Huang <khuan042@uottawa.ca>
2021-05-15 17:19:05 -0400

    removed hard coded recipes

--- recipe-book/src/app/recipes/recipe.service.ts
+++ recipe-book/src/app/recipes/recipe.service.ts
@@ -10,24 +10,27 @@ export class RecipeService {
     recipeSelected = new Subject<Recipe>();
     recipeChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Liangpi', 
-        'Shangxi Cold Noodles', 
-        'https://www.chinasichuanfood.com/wp-content/uploads/2015/09/Liangpi-Shangxi-Cold-Noodles-11-copy.jpg', 
-        [
-            new Ingredient('Rice Noodle', 1),
-            new Ingredient('Szechuan sauce', 1)
-        ]),
-        new Recipe('Beyond Burger', 
-        'Plant-based burger using Beyond Burger', 
-        'https://www.beyondmeat.com/wp-content/uploads/beyondburger-e1552079697496.jpg',
-        [
-            new Ingredient('Patty', 1),
-            new Ingredient('Bun', 1),
-            new Ingredient('Tomato', 1),
-            new Ingredient('Lettuce', 1)
-        ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Liangpi', 
+    //     'Shangxi Cold Noodles', 
+    //     'https://www.chinasichuanfood.com/wp-content/uploads/2015/09/Liangpi-Shangxi-Cold-Noodles-11-copy.jpg', 
+    //     [
+    //         new Ingredient('Rice Noodle', 1),
+    //         new Ingredient('Szechuan sauce', 1)
+    //     ]),
+    //     new Recipe('Beyond Burger', 
+    //     'Plant-based burger using Beyond Burger', 
+    //     'https://www.beyondmeat.com/wp-content/uploads/beyondburger-e1552079697496.jpg',
+    //     [
+    //         new Ingredient('Patty', 1),
+    //         new Ingredient('Bun', 1),
+    //         new Ingredient('Tomato', 1),
+    //         new Ingredient('Lettuce', 1)
+    //     ])
+    //   ];
+
+    private recipes: Recipe[] = [];
+    
       constructor(private slService: ShoppingListService) {}
       getRecipes() {
           return this.recipes.slice();
