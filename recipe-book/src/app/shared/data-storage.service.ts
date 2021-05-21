d43fe51a356c64584910d6a1425fa7be4797702a
Kevin Huang <khuan042@uottawa.ca>
2021-05-20 21:55:29 -0400

    enable fetching and saving data when user is authenticated

--- recipe-book/src/app/shared/data-storage.service.ts
+++ recipe-book/src/app/shared/data-storage.service.ts
@@ -1,15 +1,16 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, take, tap, exhaustMap } from 'rxjs/operators';
 
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({ providedIn: 'root' })
 
 export class DataStorageService {
     
-    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -22,15 +23,17 @@ export class DataStorageService {
 
     fetchRecipes(){
         return this.http.get<Recipe[]>('https://ng-my-recipe-book-5afec-default-rtdb.firebaseio.com/recipes.json')
-        .pipe(map(recipes => {
-            return recipes.map(recipe => {
-                return {
-                    ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
-                };
-            });
-        }), 
-        tap(recipes => {
-            this.recipeService.setRecipes(recipes);
-        }));
+        .pipe(
+            map(recipes => {
+                return recipes.map(recipe => {
+                    return {
+                        ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []
+                    };
+                });
+            }), 
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes);
+            })
+        );
     }
 }
