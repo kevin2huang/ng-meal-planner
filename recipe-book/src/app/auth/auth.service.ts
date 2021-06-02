bf8f705ab7ae90ef5bf529529f8e6e6ed28a76ba
Kevin Huang <khuan042@uottawa.ca>
2021-06-01 20:29:57 -0400

    build autologout feature

--- recipe-book/src/app/auth/auth.service.ts
+++ recipe-book/src/app/auth/auth.service.ts
@@ -19,7 +19,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
     user = new BehaviorSubject<User|null>(null);
-    
+    private tokenExpirationTimer: any;
 
     constructor(private http: HttpClient, private router: Router) {}
 
@@ -63,12 +63,24 @@ export class AuthService {
 
         if (loadedUser.token) {
             this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
         }
     }
 
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer)
+        }
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
@@ -81,6 +93,7 @@ export class AuthService {
         );
 
         this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
 
